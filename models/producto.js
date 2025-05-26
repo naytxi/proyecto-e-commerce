7a829e2eb7ca82ac2fa1c4e748abd76a48d89e41
Nahia Otegui <nay.fullstack@gmail.com>
2025-05-26 20:06:00 +0200

    añado las primeras tablas con sequelize, con migracion y modelos

---
+++ models/producto.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class producto extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  producto.init({
+    name: DataTypes.STRING,
+    price: DataTypes.FLOAT
+  }, {
+    sequelize,
+    modelName: 'producto',
+  });
+  return producto;
+};
