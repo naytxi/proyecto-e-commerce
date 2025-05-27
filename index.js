ad7f4d86a64498369addde57932de63341e67ff7
unknown <elimila.rodriguez@gmail.com>
2025-05-27 22:48:56 +0200

    feat: CRUD de categorías creado y funcionando (falta validar relación)

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express")
 const app = express()
 const port = 3000
+const categoriaRoutes = require('./routes/CategoriaRoutes');
+
+app.use(express.json());
+app.use('/categories', categoriaRoutes); 
+
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
---
+++ routes/CategoriaRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const CategoryController = require('../controller/CategoryController');
+
+router.post('/', CategoryController.create);
+router.get('/', CategoryController.getAll);
+router.get('/id/:id', CategoryController.getById);
+router.get('/name/:name', CategoryController.getByName);
+router.put('/id/:id', CategoryController.update);
+router.delete('/id/:id', CategoryController.delete);
+
+module.exports = router;
