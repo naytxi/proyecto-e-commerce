66c33e1407c2906c074a6796071b5a2b59817e95
Nahia Otegui <nay.fullstack@gmail.com>
2025-05-27 17:27:04 +0200

    añado rutas en index.js y cambios en las productroutes

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const express = require("express")
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+
+
+app.use(express.json());
+
+
+const productoRoutes = require('./routes/ProductoRoutes');
+app.use('/productos', productoRoutes); 
+
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`)
-})
-
+  console.log(`Server is running on http://localhost:${port}`);
+});
--- routes/ProductoRoutes.js
+++ routes/ProductoRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const ProductoController = require('../controllers/ProductoController')
-const auth = require('../middlewares/auth')
 
-router.post('/', auth, ProductoController.create) 
-router.put('/:id', auth, ProductoController.update)   
-router.delete('/:id', auth, ProductoController.delete) 
+
+router.post('/', ProductoController.create) 
+router.put('/:id', ProductoController.update)   
+router.delete('/:id', ProductoController.delete) 
 router.get('/', ProductoController.getAll)             
 router.get('/:id', ProductoController.getById)          
 
