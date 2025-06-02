0e1ad8045605c7c61ae381b360f17366784f00e6
unknown <elimila.rodriguez@gmail.com>
2025-06-02 07:48:17 +0200

    feat: middleware isAuth creado y funcionando

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const router = express.Router()
 const UserController = require('../controllers/UserController')
+const isAuth = require('../middlewares/auth') // Importamos el middleware
 
 // Rutas públicas
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 
+// Ruta privada de prueba con token
+router.get('/privado', isAuth, (req, res) => {
+  res.json({
+    message: 'Ruta privada accedida con éxito',
+    user: req.user
+  })
+})
+
 // Rutas protegidas (las usaremos más adelante)
 // router.get('/', isAuth, isAdmin, UserController.getAll)
 
 module.exports = router
+
