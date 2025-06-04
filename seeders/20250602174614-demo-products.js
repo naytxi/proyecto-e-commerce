4987e52447c2e2f22bcb821a31b2c9a67d49f401
Nahia Otegui <nay.fullstack@gmail.com>
2025-06-04 11:48:25 +0200

    añado seeder de la tabla terciaria, aunque tengo que mirar porque se han duplicado

--- seeders/20250602174614-demo-products.js
+++ seeders/20250602174614-demo-products.js
@@ -5,35 +5,30 @@ module.exports = {
     await queryInterface.bulkInsert('productos', [
       {
         name: 'Cargador USB',
-        description: 'Cargador USB de alta velocidad',
         price: 19.99,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Mantel de papel',
-        description: 'Mantel de papel desechable para fiestas',
         price: 9.99,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Alfombrilla raton',
-        description: 'Alfombrilla de ratón ergonómica',
         price: 20.99,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Ventilador portátil',
-        description: 'Ventilador portátil recargable',
         price: 19.99,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: 'Pantalla LED 24"',
-        description: 'Pantalla LED de 24 pulgadas Full HD',
         price: 159.99,
         createdAt: new Date(),
         updatedAt: new Date()
