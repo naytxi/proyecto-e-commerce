96a3be7b1a8cf0a03ea30b610023b74ebbc0f5a3
unknown <elimila.rodriguez@gmail.com>
2025-06-06 20:27:51 +0200

    muter

--- models/producto.js
+++ models/producto.js
@@ -9,25 +9,28 @@ module.exports = (sequelize, DataTypes) => {
         as: 'categories',
         foreignKey: 'productoId'
       })
-      
+
       Producto.belongsToMany(models.Pedido, {
         through: models.PedidoProducto,
         foreignKey: 'productoId',
         otherKey: 'pedidoId',
         as: 'pedidos'
-})
-
+      })
     }
   }
 
   Producto.init({
     name: DataTypes.STRING,
-    price: DataTypes.FLOAT
+    price: DataTypes.FLOAT,
+    image: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'Producto',
     tableName: 'productos'
-  });
+  })
 
-  return Producto;
-};
+  return Producto
+}
+
+
+
