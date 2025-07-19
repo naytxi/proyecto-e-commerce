12ba7b0712ac770b6bea3c89d0d8489a1d7103d6
unknown <elimila.rodriguez@gmail.com>
2025-07-19 09:37:31 +0200

    solución conflictos toker

--- models/token.js
+++ models/token.js
@@ -4,40 +4,6 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Token extends Model {
     static associate(models) {
-     
-      Token.belongsTo(models.User, {
-        foreignKey: 'userId',
-        as: 'user',
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE'
-      });
-    }
-  }
-
-  Token.init({
-    token: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    }
-  }, {
-    sequelize,
-    modelName: 'Token',
-    tableName: 'Tokens',
-  });
-
-  return Token;
-};
-
-'use strict';
-const { Model } = require('sequelize');
-
-module.exports = (sequelize, DataTypes) => {
-  class Token extends Model {
-    static associate(models) {
       Token.belongsTo(models.User, {
         foreignKey: 'userId',
         as: 'usuario'
